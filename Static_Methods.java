ebbfabfa6cc4bb22ff00bbfc8d41fb44f2ec89db
Aditi Patni <91011940+aditipatni3@users.noreply.github.com>
2025-09-04 20:19:03 +0530

    Create Static_Methods.java
    Java program to create a class called "MathUtility" with a static method add that takes two integers and returns their sum. Demonstrate the usage of this static method in the main method without creating an instance of "MathUtility".

---
+++ Static_Methods.java
@@ -0,0 +1,11 @@
+class MathUtility{
+  static int add(int a, int b){
+    int sum= a+b;
+    return sum;
+  }
+}
+public class Main{
+  public static void main(String args[]){
+   System.out.println( MathUtility.add(1, 2));
+  } 
+}

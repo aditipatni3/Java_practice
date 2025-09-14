28530c0926e973318cb0341e551f2f1331a3eada
Aditi Patni <91011940+aditipatni3@users.noreply.github.com>
2025-09-14 17:06:35 +0530

    Create Create_And_print_ArrayList.java
    Java program to create an array list, add some colors (strings) and print out the collection.

---
+++ Create_And_print_ArrayList.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+class Main{
+  public static void main(String[] args){
+    ArrayList<String> colors= new ArrayList<String>();
+      colors.add("Red");
+      colors.add("Pink");
+      colors.add("Vermilion");
+      colors.add("Hot Pink");
+      
+      
+      for(String i: colors){
+        System.out.println(i);
+      }
+  }
+}
